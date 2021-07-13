8da54623546503932809afaa24bcde1890e14c3c
EmmanuelObi <63916312+EmmanuelObi@users.noreply.github.com>
2021-07-13 06:29:38 +0100

    uploaded assignment solution

---
+++ Algorithms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System; 
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome Student,");
+           Console.Write("Are you a Fulltime or Part-time student? :  ");
+
+           string studentStatus = Console.ReadLine();
+           studentStatus = studentStatus.ToLower();
+
+           if (studentStatus == "fulltime")
+           {
+               Console.Write("What is your Course of Study? : ");
+               string courseOfStudy = Console.ReadLine();
+               Console.WriteLine($"{courseOfStudy} is a good course.");
+           }
+           else if (studentStatus == "part-time") {
+               Console.Write("How many credits are you taking? : ");
+               int amountOfCredits = Convert.ToInt32(Console.ReadLine());
+               if (amountOfCredits > 6)
+               {
+                   Console.WriteLine("That's too much Units for a part-time student");
+               }
+               else
+               {
+                   Console.WriteLine("Proceed to Screen");
+               }
+           }else
+           {
+               Console.WriteLine("Error!! \nInvalid status");
+           }
+        }
+    }
+}
---
+++ simultaneousEquation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace simultaneousEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given a Linear Equation of the form\n ax + by = e \n cx + dy = f \n solving for unknown values X and Y");
+            Console.WriteLine("Enter your values for a, b, e, c, d, f");
+            Console.Write("a : ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b : ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("e : ");
+            double e = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c : ");
+            double c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("d : ");
+            double d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("f : ");
+            double f = Convert.ToInt32(Console.ReadLine());
+
+            double x = (e * d - b * f)/ (a * d - b * c);
+            double y = (a * f - e * c)/ (a * d - b * c);
+
+            if (a * d - b * c == 0) {
+                Console.WriteLine("This Equation has no Solution");
+            }
+            else {
+                Console.WriteLine($"X = {x}, Y = {y}");
+            }
+            
+        }
+    }
+}
